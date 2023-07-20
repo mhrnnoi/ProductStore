0eede4ea66f3dff0a9ec6cf3efaa27e45710238a
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-20 12:03:32 +0330

    feat : write tests for addproductcommandhandler

---
+++ Content/tests/UnitTests/ProductStore.ApplicationTests/Products/Commands/Add/AddProductCommandHandlerTests.cs
@@ -0,0 +1,19 @@
+namespace ProductStore.ApplicationTests.Products.Commands.Add;
+
+public class AddProductCommandHandlerTests
+{
+    [Fact]
+    public void Handle_ShouldReturnCreatedProduct_WhenInputIsValid_And_HaveUniqueEmailAndDate()
+    {
+
+    }
+    public void Handle_ShouldReturnBadRequest_WhenInputIsValid_but_NoUniqueEmailAndDate()
+    {
+
+    }
+    public void Handle_ShouldReturnBadRequest_WhenInputIsNotValid()
+    {
+
+    }
+  
+}
