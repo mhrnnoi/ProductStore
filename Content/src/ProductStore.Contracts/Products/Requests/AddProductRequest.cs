59c2b832bc9718ea818d8f1b1baf128efbf2af77
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-20 18:18:02 +0330

    feat : added addproduct request contract

---
+++ Content/src/ProductStore.Contracts/Products/Requests/AddProductRequest.cs
@@ -0,0 +1,8 @@
+namespace ProductStore.Contracts.Products.Requests;
+
+public record AddProductRequest(bool IsAvailable,
+                              string ManufactureEmail,
+                              string ManufacturePhone,
+                              DateTime ProduceDate,
+                              string Name);
+
