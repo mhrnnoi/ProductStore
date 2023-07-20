6769f7aed49e1ba14236a39a5fec548af0c6f41d
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-20 12:27:01 +0330

    feat : add AAA

--- Content/tests/UnitTests/ProductStore.ApplicationTests/Products/Commands/Add/AddProductCommandHandlerTests.cs
+++ Content/tests/UnitTests/ProductStore.ApplicationTests/Products/Commands/Add/AddProductCommandHandlerTests.cs
@@ -5,7 +5,11 @@ public class AddProductCommandHandlerTests
     [Fact]
     public void Handle_ShouldReturnCreatedProduct_WhenInputIsValid_And_HaveUniqueEmailAndDate()
     {
+        //Arrange
+        
+        //Act
 
+        //Assert
     }
     public void Handle_ShouldReturnBadRequest_WhenInputIsValid_but_NoUniqueEmailAndDate()
     {
