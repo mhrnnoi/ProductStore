e1e7abfe700d877b2fdc947543f405fc4d9cd3fd
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-20 18:19:24 +0330

    feat : add problem service in api to match error

--- Content/src/ProductStore.Api/Controllers/ApiController.cs
+++ Content/src/ProductStore.Api/Controllers/ApiController.cs
@@ -1,3 +1,4 @@
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProductStore.Api.Controllers;
@@ -6,5 +7,20 @@ namespace ProductStore.Api.Controllers;
 [Route("api")]
 public class ApiController : ControllerBase
 {
+    protected IActionResult Problem(List<Error> errors)
+    {
+        var firstError = errors.First();
 
+        var myStatusCode = firstError.Type switch
+        {
+            ErrorType.Validation => StatusCodes.Status422UnprocessableEntity,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Unexpected => StatusCodes.Status405MethodNotAllowed,
+            ErrorType.Failure => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+        return Problem(statusCode: myStatusCode,
+                         title: firstError.Description);
+    }
 }
--- Content/src/ProductStore.Api/Controllers/ProductController.cs
+++ Content/src/ProductStore.Api/Controllers/ProductController.cs
@@ -1,23 +1,28 @@
+using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ProductStore.Application.Features.Products.Commands.Add;
+using ProductStore.Contracts.Products.Requests;
 
 namespace ProductStore.Api.Controllers;
 
 [Route("api/[controller]/[action]")]
 public class ProductController : ApiController
 {
+    private readonly IMapper _mapper;
+
     private readonly IMediator _mediatR;
-    public ProductController(IMediator mediatR)
+    public ProductController(IMediator mediatR, IMapper mapper)
     {
         _mediatR = mediatR;
+        _mapper = mapper;
     }
 
 
     [HttpPost]
     public async Task<IActionResult> AddProductAsync([FromBody] AddProductRequest request)
     {
-        var command = new AddProductCommand();
+        var command = _mapper.Map<AddProductCommand>(request);
 
         var result = await _mediatR.Send(command);
         return result.Match(result => Ok(result),
