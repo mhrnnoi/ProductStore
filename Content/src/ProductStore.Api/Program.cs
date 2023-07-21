e2c4b6ae1a08b9ea70a16113b75e28717fe1b706
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-21 12:01:24 +0330

    feat : config auto migration

--- Content/src/ProductStore.Api/Program.cs
+++ Content/src/ProductStore.Api/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using ProductStore.Api;
 using ProductStore.Application;
 using ProductStore.Infrastructure;
+using ProductStore.Infrastructure.Persistence.DataContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +10,21 @@ builder.Services.AddApplication()
                 .AddInfrastructure()
                 .AddPresentation();
 
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+
+    
+    dbContext.Database.Migrate();
+
+
+}
 app.Run();
