a67ad90c8ff705e1b3c3c42ab2b4a722b468b04c
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-22 18:06:34 +0330

    refactor : refactor jwtgenerator

--- Content/src/ProductStore.Infrastructure/Services/JwtGenerator.cs
+++ Content/src/ProductStore.Infrastructure/Services/JwtGenerator.cs
@@ -9,41 +9,44 @@ using ProductStore.Application.Interfaces.Services;
 
 namespace ProductStore.Infrastructure.Services;
 
-    public class JwtGenerator : IJwtGenerator
+public class JwtGenerator : IJwtGenerator
+{
+    private readonly JwtSettings _jwtOptions;
+    private IDateTimeProvider _dateTimeProvider;
+
+    public JwtGenerator(IOptions<JwtSettings> jwtOptions,
+                            IDateTimeProvider dateTimeProvider)
     {
-        private readonly JwtSettings _jwtOptions;
-        private IDateTimeProvider _dateTimeProvider;
+        _jwtOptions = jwtOptions.Value;
+        _dateTimeProvider = dateTimeProvider;
+    }
+    public string GenerateToken(IdentityUser user)
+    {
+        var key = Encoding.UTF8.GetBytes(_jwtOptions.Secret);
+        var mySigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha512);
+        Claim[] myCliaims = SetClaims(user);
 
-        public JwtGenerator(IOptions<JwtSettings> jwtOptions,
-                                IDateTimeProvider dateTimeProvider)
+        var securityToken = new JwtSecurityToken(claims: myCliaims,
+                                                 audience: _jwtOptions.Audience,
+                                                 signingCredentials: mySigningCredentials,
+                                                 expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtOptions.ExpiryMinutes));
+
+
+        var handler = new JwtSecurityTokenHandler();
+        return handler.WriteToken(securityToken);
+    }
+
+    private Claim[] SetClaims(IdentityUser user)
+    {
+        return new[]
         {
-            _jwtOptions = jwtOptions.Value;
-            _dateTimeProvider = dateTimeProvider;
-        }
-        public string GenerateToken(IdentityUser user)
-        {
-            var key = Encoding.UTF8.GetBytes(_jwtOptions.Secret);
-            var mySigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                                                                SecurityAlgorithms.HmacSha512);
-
-
-            var myCliaims = new[]
-            {
+                new Claim("id", user.Id),
                 new Claim(JwtRegisteredClaimNames.Sub,"Token"),
                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
                 new Claim(JwtRegisteredClaimNames.Iat, _dateTimeProvider.UtcNow.ToString(CultureInfo.InvariantCulture)),
-                new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.Email, user.Email),
             };
-            
-            var securityToken = new JwtSecurityToken(claims: myCliaims,
-                                                     audience: _jwtOptions.Audience,
-                                                     signingCredentials: mySigningCredentials,
-                                                     expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtOptions.ExpiryMinutes));
-
-
-            var handler = new JwtSecurityTokenHandler();
-            return handler.WriteToken(securityToken);
-        }
     }
+}
